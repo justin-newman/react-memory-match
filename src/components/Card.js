07a6c2f31f6c82ba0926f4fc491348ffb6c09cc2
Shannon Byland <shannonbyland@gmail.com>
2017-05-16 19:28:07 -0600

    updated comments

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,14 +1,14 @@
 import React from 'react';
 
 const Card = ({ updateCard, flipped, matched, icon, index }) => (
-  <div 
-    className='col-xs-3 text-center' 
+  <div
+    className='col-xs-3 text-center'
     style={{ height: '300px', border: '1px solid black'}}
   >
    {/*need a onClick handler on the card div to call updateCard*/}
-   
+
    {/*show icon if flipped or matched*/}
-   {/*<i className={`fa ${icon} fa-5x`} />*/}
+  //  <i className={`fa ${icon} fa-5x`} />
 
    {/*do not show icon if not flipped or matched*/}
 
--- src/components/Game.js
+++ src/components/Game.js
@@ -5,26 +5,36 @@ class Game extends Component {
   // Match Icons: fa-bug, fa-bolt, fa-cloud-upload, fa-cloud-download
 
   cards = [
-    { flipped: false, matched: false, icon: 'fa-bug'}, 
-    { flipped: false, matched: false, icon: 'fa-bolt'}, 
-    { flipped: false, matched: false, icon: 'fa-bug'}, 
+    { flipped: false, matched: false, icon: 'fa-bug'},
     { flipped: false, matched: false, icon: 'fa-bolt'},
-    { flipped: false, matched: false, icon: 'fa-cloud-upload'}, 
-    { flipped: false, matched: false, icon: 'fa-cloud-download'}, 
+    { flipped: false, matched: false, icon: 'fa-bug'},
+    { flipped: false, matched: false, icon: 'fa-bolt'},
     { flipped: false, matched: false, icon: 'fa-cloud-upload'},
-    { flipped: false, matched: false, icon: 'fa-cloud-download'} 
+    { flipped: false, matched: false, icon: 'fa-cloud-download'},
+    { flipped: false, matched: false, icon: 'fa-cloud-upload'},
+    { flipped: false, matched: false, icon: 'fa-cloud-download'}
   ]
-  ''
-  state = { cards: this.cards, flippedCardIndexes: [], matchedCardIndexs: [] };
+
+  // use flippedCardIndexes to check how many cards are flipped
+  // used matchedCardIndexes to check if count === 8 || this.state.cards.length ->then gameOver
+  state = { cards: this.cards, flippedCardIndexes: [], matchedCardIndexes: [] };
 
   // What lifecycle method could we use each time the components state is updated to check for gameOver
+// ANSWER: didUpdate
 
   // figure out how to end the game once all cards are matched this probably requires more state or a lifecycle method that loops card state
   // figure out how to start a new game
   // figure out how to change the players username while in the game
+   // ANSWER: just setState
+
+   //BONUS
+   // figure out how to randomize cards on new game (HINT: look up javascript shuffle array)
+   // Use images from images folder instead of fontawesome icons
   // implement a game timer and save fastest times
   // integrate this project into an express app and store this info to a database
 
+
+// on card click-- needs to be passed as props to card.js
   updateCard = (cardIndex, flipped = false, matched = false) => {
     let cards = this.state.cards.map( (card, loopIndex) => {
       if(cardIndex === loopIndex)
